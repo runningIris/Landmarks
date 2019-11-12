8c2d562bd845d10be43cff0e439966ddc3699a10
runningiris <irisrunning@foxmail.com>
2019-11-12 13:52:04 +0800

    init

---
+++ Landmarks/Supporting Views/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Iris on 2019/11/12.
+//  Copyright © 2019 Iris. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
